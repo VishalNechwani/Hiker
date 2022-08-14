5348082abd451f503efa7fcf7c6eca50a342ff58
Vishal Nechwani <nechwanivishal97@gmail.com>
2022-08-14 16:52:33 +0530

    UI Changes

--- app/src/main/java/com/example/hiker/adapter/CompanyListAdapter.kt
+++ app/src/main/java/com/example/hiker/adapter/CompanyListAdapter.kt
@@ -1,10 +1,10 @@
 package com.example.hiker.adapter
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.graphics.Color
+import android.view.*
 import android.widget.ListView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hiker.R
 import com.example.hiker.model.HikeEntity
@@ -12,6 +12,8 @@ import com.example.hiker.model.HikeEntity
 
 class CompanyListAdapter(val hikeList:List<HikeEntity>) : RecyclerView.Adapter<CompanyListAdapter.ViewHolder>()  {
 
+
+    var isEnable = false
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -23,7 +25,15 @@ class CompanyListAdapter(val hikeList:List<HikeEntity>) : RecyclerView.Adapter<C
         holder.companyName.text = hikeList.get(position).company_name
         holder.inHandSalary.text = hikeList.get(position).inHandNew
         holder.componentList.adapter = CompanyListSubComponentAdapter(hikeList.get(position).component_arr)
+        holder.card.isLongClickable = true
     }
+
+    private fun clickItem(holder: CompanyListAdapter.ViewHolder) {
+        val adapter = holder.adapterPosition
+        holder.card.setBackgroundColor(Color.GRAY)
+    }
+
+
 
     override fun getItemCount(): Int {
         return hikeList.size;
@@ -31,9 +41,11 @@ class CompanyListAdapter(val hikeList:List<HikeEntity>) : RecyclerView.Adapter<C
 
     // Holds the views for adding it to image and text
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val card: CardView = itemView.findViewById(R.id.card_view)
         val companyName: TextView = itemView.findViewById(R.id.company_name)
         val inHandSalary: TextView = itemView.findViewById(R.id.in_hand_salary)
         val componentList:RecyclerView = itemView.findViewById(R.id.component_list)
 
     }
 }
+
